c7be4c65fdf01817b7395306556552f5fd22c6b2
ANKUSH-JPG <51692515+ANKUSH-JPG@users.noreply.github.com>
2020-06-18 11:59:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-print "************* HELLO GUYS!!!! THIS IS ANKUSH SAINI ******************** ";
-print "--------------------- I WELCOME YOU ALL ------------------------";
+printasdfa "************* HELLO GUYS!!!! THIS IS ANKUSH SAINI ******************** ";
+printasf "--------------------- I WELCOME YOU ALL ------------------------";
 ?>
